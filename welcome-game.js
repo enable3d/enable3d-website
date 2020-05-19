cbbccf2c05ecfecd85eb25880de9c27a3aabf1c1
Yannick <20306025+yandeu@users.noreply.github.com>
2020-05-19 22:20:29 +0200

    Fixed the position of the joystick

--- welcome-game.js
+++ welcome-game.js
@@ -184,7 +184,7 @@ class MainScene extends Scene3D {
     if (isTouchDevice) {
       const joystick = new JoyStick()
       const axis = joystick.add.axis({
-        styles: { left: 35, bottom: 35, size: 100 }
+        styles: { left: 20, bottom: 175, size: 100 }
       })
       axis.onMove(event => {
         /**
@@ -196,12 +196,12 @@ class MainScene extends Scene3D {
       })
       const buttonA = joystick.add.button({
         letter: 'A',
-        styles: { right: 35, bottom: 110, size: 80 }
+        styles: { right: 20, bottom: 250, size: 80 }
       })
       buttonA.onClick(() => this.jump())
       const buttonB = joystick.add.button({
         letter: 'B',
-        styles: { right: 110, bottom: 35, size: 80 }
+        styles: { right: 95, bottom: 175, size: 80 }
       })
       buttonB.onClick(() => (this.move = true))
       buttonB.onRelease(() => (this.move = false))
