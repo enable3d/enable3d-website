bff4d1216325eff650918505a29a6cd4b79ddd20
yandeu <20306025+yandeu@users.noreply.github.com>
2021-11-26 21:10:16 +0100

    Minor improvements

--- src/js/load-images.js
+++ src/js/load-images.js
@@ -1,25 +1,20 @@
-// https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video
-
-let lazyImages = [].slice.call(document.querySelectorAll('img.lazy'))
-let active = false
-
-const lazyLoad = function () {
-  setTimeout(function () {
-    lazyImages.forEach(function (lazyImage) {
-      if (
-        lazyImage.getBoundingClientRect().top <= window.innerHeight &&
-        lazyImage.getBoundingClientRect().bottom >= 0 &&
-        getComputedStyle(lazyImage).display !== 'none'
-      ) {
-        lazyImage.src = lazyImage.dataset.src
-        if (lazyImage.dataset && lazyImage.dataset.srcset) lazyImage.srcset = lazyImage.dataset.srcset
-        lazyImage.classList.remove('lazy')
-
-        lazyImages = lazyImages.filter(function (image) {
-          return image !== lazyImage
+'use strict'
+var lazyLoad = function () {
+  var lazyImages = Array.from(document.querySelectorAll('img.lazy'))
+  lazyImages.forEach(function (lazyImage) {
+    if (lazyImage.dataset && lazyImage.dataset.src) {
+      // fetch the image to the browser cache
+      fetch(lazyImage.dataset.src, { mode: 'no-cors' })
+        .then(function () {
+          // replace the new image with the placeholder
+          lazyImage.src = lazyImage.dataset.src
         })
-      }
-    })
-  }, 200)
+        .catch(function () {
+          console.warn('Failed to fetch', lazyImage.dataset.src)
+        })
+    }
+  })
 }
-lazyLoad()
+setTimeout(function () {
+  lazyLoad()
+}, 200)
