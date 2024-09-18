8a7ee43ccd131eaac40b0a3dbc405ae98904f9c5
joemichaelpower <joemichaelpower@gmail.com>
2024-09-18 19:58:22 +0000

    Commit

---
+++ src/js/cart.js
@@ -0,0 +1,8 @@
+let iconCart = document.querySelector('.icon-cart');
+let closeCart = document.querySelector('.close');
+let body = document.querySelector('body');
+
+iconCart.addEventListener('click', ()=> {
+    body.classList.toggle('showCart')
+    console.log("clicked")
+})
